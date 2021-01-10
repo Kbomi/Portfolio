d5d089e613b0abbf648e023cc6c03f31fdc5bd3a
Kbomi <bomi@edkim.co>
2021-01-10 15:37:45 +0900

    header, footer, mixin추가

---
+++ components/header/index.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import styles from "./index.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Bomi</h1>
+      <nav id="gnbMenuWrap">
+        <ul className={styles.nav_list}>
+          <li>
+            <Link href="/">
+              <a>Blog</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
