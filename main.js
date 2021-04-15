14b5ed410799d8b8844a331502059449c6ad78b5
kimbm <kimbm@brandi.co.kr>
2021-04-15 09:56:02 +0900

    초기화

--- main.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import Index from "./index";
-import NumberBaseball from "./NumberBaseball";
-import ResponseCheck from "./ResponseCheck";
-import RockScissorsPaper from "./RockScissorsPaper";
-import LottoGenerator from "./LottoGenerator";
-
-new Vue(LottoGenerator).$mount("#root");
