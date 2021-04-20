0d2e9b45eb84bb5f36f6da0df462a21931232024
kimbm <kimbm@brandi.co.kr>
2021-04-20 09:44:28 +0900

    meta tag 설정

--- src/router/index.js
+++ src/router/index.js
@@ -5,18 +5,75 @@ import BrandiComponent from '@/components/brandi/main/MainComponent'
 
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Main',
-      component: MainComponent
+      component: MainComponent,
+      meta: {
+        title: '보미 포트폴리오',
+        metaTags: [
+          {description: '보미의 포트폴리오'}
+        ]
+      }
     },
     {
       path: '/brandi',
       name: 'Brandi',
-      component: BrandiComponent
+      component: BrandiComponent,
+      meta: {
+        title: '보미 포트폴리오 - 브랜디 메인',
+        metaTags: [
+          {description: '브랜디 메인 퍼블리싱'}
+        ]
+      }
     }
   ]
 })
+
+export default router
+
+router.beforeEach((to, from, next) => {
+
+  // title 설정이 있는 경우
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+
+  // meta tag 설정이 있는 경우
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+  
+  // 이전 meta tag 설정이 있는 이전 페이지의 정보
+  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+
+  // 현재 title이 있으면 보여주고 없으면 전 페이지의 metaTag의 title을 보여줌
+  if(nearestWithTitle) {
+    document.title = nearestWithTitle.meta.title;
+  } else if(previousNearestWithMeta) {
+    document.title = previousNearestWithMeta.meta.title;
+  }
+
+  // [data-vue-router-controlled] 속성을 갖고 있는 element들 삭제.
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+
+  // meta tag 설정이 없는 경우 건너뛰기.
+  if(!nearestWithMeta) return next();
+
+  // head 안에 meta tag 생성
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta');
+
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key]);
+    });
+
+    // tag의 [data-vue-router-controlled] 속성 지우기
+    tag.setAttribute('data-vue-router-controlled', '');
+
+    return tag;
+  })
+  // document head에 appendChild 해줌.
+  .forEach(tag => document.head.appendChild(tag));
+
+  next();
+});
